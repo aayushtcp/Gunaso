27c5e4e7f48383906bbd1156105b2a0ab5bc402b
aayushdhakal69 <therespawner69@gmail.com>
2023-12-25 08:57:10 +0545

    backup for safety

---
+++ static/scripts/base2.js
@@ -0,0 +1,122 @@
+(() => {
+    const openNavMenu = document.querySelector(".open-nav-menu"),
+        closeNavMenu = document.querySelector(".close-nav-menu"),
+        navMenu = document.querySelector(".nav-menu"),
+        menuOverlay = document.querySelector(".menu-overlay"),
+        mediaSize = 991;
+
+    openNavMenu.addEventListener("click", toggleNav);
+    closeNavMenu.addEventListener("click", toggleNav);
+    // close the navMenu by clicking outside
+    menuOverlay.addEventListener("click", toggleNav);
+
+    function toggleNav() {
+        navMenu.classList.toggle("open");
+        menuOverlay.classList.toggle("active");
+        document.body.classList.toggle("hidden-scrolling");
+    }
+
+    navMenu.addEventListener("click", (event) => {
+        if (
+            event.target.hasAttribute("data-toggle") &&
+            window.innerWidth <= mediaSize
+        ) {
+            // prevent default anchor click behavior
+            event.preventDefault();
+            const menuItemHasChildren = event.target.parentElement;
+            // if menuItemHasChildren is already expanded, collapse it
+            if (menuItemHasChildren.classList.contains("active")) {
+                collapseSubMenu();
+            } else {
+                // collapse existing expanded menuItemHasChildren
+                if (navMenu.querySelector(".menu-item-has-children.active")) {
+                    collapseSubMenu();
+                }
+                // expand new menuItemHasChildren
+                menuItemHasChildren.classList.add("active");
+                const subMenu = menuItemHasChildren.querySelector(".sub-menu");
+                subMenu.style.maxHeight = subMenu.scrollHeight + "px";
+            }
+        }
+    });
+
+    function collapseSubMenu() {
+        navMenu
+            .querySelector(".menu-item-has-children.active .sub-menu")
+            .removeAttribute("style");
+        navMenu
+            .querySelector(".menu-item-has-children.active")
+            .classList.remove("active");
+    }
+
+    function resizeFix() {
+        // if navMenu is open ,close it
+        if (navMenu.classList.contains("open")) {
+            toggleNav();
+        }
+        // if menuItemHasChildren is expanded , collapse it
+        if (navMenu.querySelector(".menu-item-has-children.active")) {
+            collapseSubMenu();
+        }
+    }
+
+    window.addEventListener("resize", function() {
+        if (this.innerWidth > mediaSize) {
+            resizeFix();
+        }
+    });
+})();
+
+
+// sidebar left script
+// Selecting the sidebar and buttons
+const sidebar = document.querySelector(".sidebar");
+const sidebarOpenBtn = document.querySelector("#sidebar-open");
+const sidebarCloseBtn = document.querySelector("#sidebar-close");
+const sidebarLockBtn = document.querySelector("#lock-icon");
+
+// Function to toggle the lock state of the sidebar
+const toggleLock = () => {
+    sidebar.classList.toggle("locked");
+    // If the sidebar is not locked
+    if (!sidebar.classList.contains("locked")) {
+        sidebar.classList.add("hoverable");
+        sidebarLockBtn.classList.replace("bx-lock-alt", "bx-lock-open-alt");
+    } else {
+        sidebar.classList.remove("hoverable");
+        sidebarLockBtn.classList.replace("bx-lock-open-alt", "bx-lock-alt");
+    }
+};
+
+// Function to hide the sidebar when the mouse leaves
+const hideSidebar = () => {
+    if (sidebar.classList.contains("hoverable")) {
+        sidebar.classList.add("close");
+    }
+};
+
+// Function to show the sidebar when the mouse enter
+const showSidebar = () => {
+    if (sidebar.classList.contains("hoverable")) {
+        sidebar.classList.remove("close");
+    }
+};
+
+// Function to show and hide the sidebar
+const toggleSidebar = () => {
+    sidebar.classList.toggle("close");
+};
+
+// If the window width is less than 800px, close the sidebar and remove hoverability and lock
+if (window.innerWidth < 800) {
+    sidebar.classList.add("close");
+    sidebar.classList.remove("locked");
+    sidebar.classList.remove("hoverable");
+}
+
+// Adding event listeners to buttons and sidebar for the corresponding actions
+sidebarLockBtn.addEventListener("click", toggleLock);
+sidebar.addEventListener("mouseleave", hideSidebar);
+sidebar.addEventListener("mouseenter", showSidebar);
+sidebarOpenBtn.addEventListener("click", toggleSidebar);
+sidebarCloseBtn.addEventListener("click", toggleSidebar);
