4c48b6d0289f6a86ad8287b010fc7beebc65f3e9
Aayush <therespawner69@gmail.com>
2024-02-26 15:29:48 +0545

    signup form validated

---
+++ static/scripts/signup.js
@@ -0,0 +1,121 @@
+// to display image in div
+function displayImage(input) {
+  var file = input.files[0];
+  if (file) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      document.getElementById("profilePicContainer").style.backgroundImage =
+        "url(" + e.target.result + ")";
+    };
+    reader.readAsDataURL(file);
+  }
+}
+
+// for all validation
+let message = document.getElementById("quickmessage");
+let submitButton = document.querySelector(".copybtn");
+// to valiadte ussername
+function usernameValid() {
+  let username = document.getElementById("username").value;
+  if (!username) {
+    message.innerHTML = "Please enter your username";
+    submitButton.style.cursor = "not-allowed";
+    submitButton.disabled = true;
+  } else {
+    let usernameRegex = /^[a-zA-Z][a-zA-Z0-9]*$/;
+    if (!usernameRegex.test(username)) {
+      submitButton.style.cursor = "not-allowed";
+      message.innerHTML =
+        "Invalid username. Please follow the specified rules. <ul><li>Username must start with character.</li> <li>Username should not have spaces.</li><li>Username shoud be Unique.</li><ul>";
+      submitButton.disabled = true;
+    } else {
+      message.innerHTML = "";
+      submitButton.style.cursor = "pointer";
+      submitButton.disabled = false;
+    }
+  }
+}
+
+// to verify the intro
+function introValid() {
+  let intro = document.getElementById("intro").value;
+  if (!intro) {
+    message.innerHTML = "Please enter your intro";
+    submitButton.disabled = true;
+    submitButton.style.cursor = "not-allowed";
+  } else if (intro.length > 40) {
+    message.innerHTML = "Intro should not be more than 40 characters";
+    submitButton.disabled = true;
+    submitButton.style.cursor = "not-allowed";
+  } else {
+    message.innerHTML = "";
+    submitButton.disabled = false;
+    submitButton.style.cursor = "pointer";
+  }
+}
+
+// to verify the email
+function emailValid() {
+  let email = document.getElementById("email").value;
+  if (!email) {
+    message.innerHTML = "Please enter your email address";
+    submitButton.disabled = true;
+    submitButton.style.cursor = "not-allowed";
+  } else {
+    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      submitButton.style.cursor = "not-allowed";
+      message.innerHTML = "Invalid email. Please enter a valid email address";
+      submitButton.disabled = true;
+    } else {
+      message.innerHTML = "";
+      submitButton.style.cursor = "pointer";
+      submitButton.disabled = false;
+    }
+  }
+}
+
+// to verify the password
+function passwordValid() {
+  let password = document.getElementById("password").value;
+  if (!password) {
+    message.innerHTML = "Please enter your password";
+    submitButton.disabled = true;
+    submitButton.style.cursor = "not-allowed";
+  } else if (password.length < 8 || password.length > 32) {
+    message.innerHTML = "Password must be between 8 and 32 characters";
+    submitButton.disabled = true;
+    submitButton.style.cursor = "not-allowed";
+  } else if (!/\d/.test(password)) {
+    message.innerHTML = "Password must include at least one number";
+    submitButton.disabled = true;
+    submitButton.style.cursor = "not-allowed";
+  } else if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+    message.innerHTML = "Password must include at least one special character";
+    submitButton.disabled = true;
+    submitButton.style.cursor = "not-allowed";
+  } else if (!/[A-Z]/.test(password)) {
+    message.innerHTML = "Password must include at least one capital letter";
+    submitButton.disabled = true;
+    submitButton.style.cursor = "not-allowed";
+  } else {
+    message.innerHTML = "";
+    submitButton.style.cursor = "pointer";
+    submitButton.disabled = false;
+  }
+}
+
+// password match validation
+function passwordMatchValid() {
+  let password = document.getElementById("password").value;
+  let cpassword = document.getElementById("cpassword").value;
+  if (password != cpassword) {
+    message.innerHTML = "Password is not matching...";
+    submitButton.disabled = true;
+    submitButton.style.cursor = "not-allowed";
+  } else {
+    message.innerHTML = "";
+    submitButton.style.cursor = "pointer";
+    submitButton.disabled = false;
+  }
+}
