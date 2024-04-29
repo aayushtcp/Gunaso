c43a87d2c0cfe58f412246425f37009bcc6b9029
Aayush <therespawner69@gmail.com>
2024-04-29 19:49:24 +0545

    modified detectViolence

--- gunasoApp/detectViolence.py
+++ gunasoApp/detectViolence.py
@@ -25,23 +25,19 @@ def detect_violence(image):
     upper_red = np.array([136, 8, 8])
     # 255 90 0 
 
-    # Create a mask to isolate red regions
     mask = cv2.inRange(hsv_image, lower_red, upper_red)
 
-    # Count the number of red pixels
     red_pixel_count = cv2.countNonZero(mask)
 
-    # Define a threshold for the number of red pixels to consider as blood
-    blood_threshold = 5000  # Adjust this value based on your requirements
+    blood_threshold = 5000  
 
-    # Determine if there is blood in the image
     has_blood = red_pixel_count > blood_threshold
 
     print(f"Is violent: {is_violent}")
     print(f"Has blood level: {red_pixel_count}")
     print(f"The avg_intensity is: {avg_intensity}")
 
-    # Determine the final result based on violence and blood detection
+    # final result based on violence and blood detection
     if is_violent or has_blood:
         return True
     else:
